2b03278f15124310cd0fc028df63f82965da6c62
Brandon K. Waltz <bkwaltz@colby.edu>
2019-10-09 10:47:14 -0400

    redundent code in install-node-tools.sh

--- scripts/install-node-tools.sh
+++ scripts/install-node-tools.sh
@@ -11,8 +11,8 @@ nvm use $NODE_VERSION
 
 
 unset NPM_CONFIG_PREFIX
-        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | dash
+        curl -o- https://raw.githubusercontent.com/creationix/nvm/v{$NVM_VERSION}/install.sh | dash
         export NVM_DIR="$HOME/.nvm"
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
         nvm current
-        nvm install 9.5.0
+        nvm install $NODE_VERSION
