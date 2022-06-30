af4e5566b905cc4cc582a2266189bbb9554fbc45
Brandon K. Waltz <bwaltz6@gmail.com>
2022-06-30 12:30:13 -0400

    ci

--- scripts/sitemap_master.sh
+++ scripts/sitemap_master.sh
@@ -1,4 +1,4 @@
-EDGE_HOSTNAME=$(~/.platformsh/bin/platform domains --format=plain --no-header --columns=name )
+EDGE_HOSTNAME="afa.colby.edu"
 JQ="https://${EDGE_HOSTNAME}"
 echo $(cat .github/sitemap.json | jq -r --arg JQ "$JQ" '.urls += [$JQ]') > .github/sitemap.json
 cat .github/sitemap.json
