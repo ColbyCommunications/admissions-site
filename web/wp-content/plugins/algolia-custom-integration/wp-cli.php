720b100c82aad6ceb6a45a8428c50a2ff123b98f
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-01-26 14:47:48 -0500

    add counselor posts to algolia index

--- web/wp-content/plugins/algolia-custom-integration/wp-cli.php
+++ web/wp-content/plugins/algolia-custom-integration/wp-cli.php
@@ -1,77 +1,89 @@
 <?php
 
-if (!(defined('WP_CLI') && WP_CLI)) {
-    return;
+if ( ! ( defined( 'WP_CLI' ) && WP_CLI ) ) {
+	return;
 }
 
 class Algolia_Command {
-    public function reindex_post($args, $assoc_args) {
-        global $algolia;
-        $index = $algolia->initIndex('Admissions');
-        
-        //Clear previous index
-        $index->deleteBy(['filters'=>'type:wp'])->wait();
-            
-        $paged = 1;
-        $count = 0;
-  
-        do {
-            $posts = new WP_Query([
-                'posts_per_page' => 100,
-                'paged' => $paged,
-                'post_type' => ['page','post']
-            ]);
-  
-            if (!$posts->have_posts()) {
-                break;
-            }
-  
-            $records = $entries = [];
-  
-            foreach ($posts->posts as $post) {
+	public function reindex_post( $args, $assoc_args ) {
+		global $algolia;
+		$index = $algolia->initIndex( 'Admissions' );
 
-                $post_tags = wp_get_post_tags($post->ID);
-                $tag_name_array=[];
-                $tags='';
-                if (count($post_tags)){
-                    foreach ($post_tags as $post_tag){
-                        $tag_name_array[]=$post_tag->name;
-                    }
-                    $tags=implode(', ',$tag_name_array);
-                }
-                
+		//Clear previous index
+		$index->deleteBy( array( 'filters' => 'type:wp' ) )->wait();
 
-                if (isset($assoc_args['verbose']) && $assoc_args['verbose']) {
-                    WP_CLI::line('Serializing ['.$post->post_title.']');
-                }
-                $record = (array) apply_filters('post_to_record', $post);
-                
-                
-                $entry=[];
-                $entry['title'] = $record['post_title'];
-                $entry['description'] = strip_shortcodes($record['post_content']);
-                $entry['url'] = get_permalink($post);
-                $entry['tags'] = $tags;
-                $entry['type'] = 'wp';
-                $entry['id'] = $entry['objectID'] = implode('-', ["wp",$post->post_type, $post->ID]);
-                $records[] = $entry;
+		$paged = 1;
+		$count = 0;
 
-                $count++;
-            }
-  
-            if (isset($assoc_args['verbose']) && $assoc_args['verbose']) {
-                WP_CLI::line('Sending batch');
-            }
-  
-            $index->saveObjects($records);
-  
-            $paged++;
-  
-        } while (true);
-  
-        WP_CLI::success("$count posts indexed in Algolia");
-    }
-  }
-  
+		do {
+			$posts = new WP_Query(
+				array(
+					'posts_per_page' => 100,
+					'paged'          => $paged,
+					'post_type'      => array( 'page', 'post', 'counselors' ),
+				)
+			);
 
-WP_CLI::add_command('algolia', 'Algolia_Command');
+			if ( ! $posts->have_posts() ) {
+				break;
+			}
+
+			$records = $entries = array();
+
+			foreach ( $posts->posts as $post ) {
+
+				$post_tags      = wp_get_post_tags( $post->ID );
+				$tag_name_array = array();
+				$tags           = '';
+				if ( count( $post_tags ) ) {
+					foreach ( $post_tags as $post_tag ) {
+						$tag_name_array[] = $post_tag->name;
+					}
+					$tags = implode( ', ', $tag_name_array );
+				}
+
+				if ( isset( $assoc_args['verbose'] ) && $assoc_args['verbose'] ) {
+					WP_CLI::line( 'Serializing [' . $post->post_title . ']' );
+				}
+				$record = (array) apply_filters( 'post_to_record', $post );
+
+				$entry = array();
+				if ( get_post_type( $post ) !== 'counselors' ) {
+					$entry['title']       = $record['post_title'];
+					$entry['description'] = strip_shortcodes( $record['post_content'] );
+					$entry['url']         = get_permalink( $post );
+					$entry['tags']        = $tags;
+				} else {
+					$counselor_post_meta  = get_post_meta( $post->ID );
+					$first_name           = $counselor_post_meta['first_name'][0];
+					$last_name            = $counselor_post_meta['last_name'][0];
+					$job_title            = $counselor_post_meta['job_title'][0];
+					$email                = $counselor_post_meta['email'][0];
+					$phone                = $counselor_post_meta['phone'][0];
+					$entry['title']       = "{$first_name} {$last_name}";
+					$entry['description'] = "{$first_name} {$last_name} - {$job_title} - {$email} - {$phone}";
+					$entry['url']         = 'https://afa.colby.edu/counselors/';
+				}
+				$entry['type'] = 'wp';
+				$entry['id']   = $entry['objectID'] = implode( '-', array( 'wp', $post->post_type, $post->ID ) );
+				$records[]     = $entry;
+
+				$count++;
+			}
+
+			if ( isset( $assoc_args['verbose'] ) && $assoc_args['verbose'] ) {
+				WP_CLI::line( 'Sending batch' );
+			}
+
+			$index->saveObjects( $records );
+
+			$paged++;
+
+		} while ( true );
+
+		WP_CLI::success( "$count posts indexed in Algolia" );
+	}
+}
+
+
+WP_CLI::add_command( 'algolia', 'Algolia_Command' );
