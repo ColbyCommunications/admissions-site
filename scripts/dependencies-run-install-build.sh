1949f7da9ce3f067cfa00fc8e6241242e72b73e0
Brandon K. Waltz <bwaltz6@gmail.com>
2023-09-29 11:10:27 -0400

    adds theme build to site

--- scripts/dependencies-run-install-build.sh
+++ scripts/dependencies-run-install-build.sh
@@ -7,6 +7,13 @@ shopt -s extglob # Turns on extended globbing
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
+cd web/wp-content/themes/colby-admissions-theme
+composer install
+composer dump-autoload
+npm install
+npm run build-prod
+cd -
+
 cd web/wp-content/plugins/colby-counselors
 composer install
 composer dump-autoload
