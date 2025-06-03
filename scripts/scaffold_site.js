848919fdbe5368f0dc5c22dcde173875d7fcac8b
Brandon K. Waltz <bwaltz6@gmail.com>
2025-06-03 13:15:54 -0400

    maintenance site abstraction

---
+++ scripts/scaffold_site.js
@@ -0,0 +1,12 @@
+// do we have files?
+    // yes: we're processing an existing repo
+    // no: we're building new site
+// build site_specific folder
+    // build out lando folder
+    // build out platform folder
+    // build empty test folder
+    // generate composer_requirements.json
+    // generate robots.txt
+// if new site: generate files from starter
+// if existing repo: generate file edits
+
