1df44a9d93d7f92692cdbbc369b9de20e2c2be47
Brandon K. Waltz <bwaltz6@gmail.com>
2025-06-16 15:19:16 -0400

    Update maintenance.js

--- scripts/maintenance.js
+++ scripts/maintenance.js
@@ -4,31 +4,26 @@ const { execSync } = require('child_process');
  * Create new git branch
  */
 
-execSync('git branch --show-current', (err, stdout, stderr) => {
-    if (err) {
-        console.error('Error running git command:', err);
-        return;
-    }
-    const branch = stdout.trim();
-    if (branch === 'dev') {
-        // Get current month and year
-        const now = new Date();
-        const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-        const year = now.getFullYear();
+const branch = execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf8' }).trim();
 
-        const branchName = `maintenance-${month}-${year}`;
+if (branch === 'dev') {
+    // Get current month and year
+    const now = new Date();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const year = now.getFullYear();
 
-        try {
-            // Create and checkout the new branch
-            execSync(`git checkout -b ${branchName}`, { stdio: 'inherit' });
-            console.log(`Switched to new branch: ${branchName}`);
-        } catch (error) {
-            console.error('Error creating or switching branch:', error.message);
-        }
-    } else {
-        console.log(`Current branch is "${branch}", not "dev".`);
+    const branchName = `maintenance-${month}-${year}`;
+
+    try {
+        // Create and checkout the new branch
+        execSync(`git checkout -b ${branchName}`, { stdio: 'inherit' });
+        console.log(`Switched to new branch: ${branchName}`);
+    } catch (error) {
+        console.error('Error creating or switching branch:', error.message);
     }
-});
+} else {
+    console.log(`Current branch is "${branch}", not "dev".`);
+}
 
 /**
  * Sync starter
@@ -44,28 +39,6 @@ execSync('git pull upstream master --allow-unrelated-histories', (err, stdout, s
  * Run composer update script
  */
 execSync('node ./scripts/composer_update.js', (err, stdout, stderr) => {
-    if (err) {
-        console.error('Error running git command:', err);
-        return;
-    }
-});
-
-/**
- * Git commit and push
- */
-execSync('git add *', (err, stdout, stderr) => {
-    if (err) {
-        console.error('Error running git command:', err);
-        return;
-    }
-});
-execSync(`git commit -m "maintenance for ${month}/${year}"`, (err, stdout, stderr) => {
-    if (err) {
-        console.error('Error running git command:', err);
-        return;
-    }
-});
-execSync('git push' , (err, stdout, stderr) => {
     if (err) {
         console.error('Error running git command:', err);
         return;
