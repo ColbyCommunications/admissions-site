2aa3de08ea66a8ccb883816142cdff933498efcb
Brandon K. Waltz <bwaltz6@gmail.com>
2025-08-11 15:55:14 -0400

    Update cleanup-dev-db.sh

--- scripts/cleanup-dev-db.sh
+++ scripts/cleanup-dev-db.sh
@@ -5,4 +5,7 @@ DEV_HOSTNAME=$(echo $PLATFORM_ROUTES | base64 --decode | jq 'keys[0]' | tr -d \"
 if [ "${PLATFORM_BRANCH}" != master ]; then
   echo "Running: wp search-replace 'https://${PRIMARY_DOMAIN}/' '${DEV_HOSTNAME}' --all-tables"
   wp search-replace "https://${PRIMARY_DOMAIN}/" "${DEV_HOSTNAME}" --all-tables
+
+  echo "Publishing Test Page"
+  wp db query 'UPDATE wp_posts SET post_status = "publish" WHERE post_title="Test Page"'
 fi
